77dbbd392712739e06a1102fa36068ff39e1068c
root <root@DESKTOP-4I5C2KQ.localdomain>
2021-01-11 22:21:01 -0500

    Crear componente imagen de comic

---
+++ src/components/Comicimage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="image">
+    <img :src="dataComic.img" alt="comicImage" />
+    <Rate />
+  </div>
+</template>
+
+<script>
+import Rate from './Rate';
+export default {
+  name: 'ComicImage',
+  props: ['dataComic'],
+  components: {
+    Rate,
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  margin: 2em 1.2em 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.image img {
+  width: 100%;
+  margin-bottom: 1.2em;
+}
+</style>
