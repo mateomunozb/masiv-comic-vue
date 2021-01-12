2edd3b08bae749ba0f7888388a95fd223b5cc9a8
root <root@DESKTOP-4I5C2KQ.localdomain>
2021-01-11 22:20:19 -0500

    Crear componente header

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cover">
+    <a href="/">
+      <h1>xkcd</h1>
+    </a>
+    <div>
+      <p>A webcomic of romance, sarcasm, math, and language.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.cover {
+  background-color: black;
+  display: flex;
+  color: white;
+  justify-content: space-around;
+  padding: 0.5em;
+  align-items: center;
+  text-align: center;
+}
+
+.cover h1 {
+  font-family: 'Gravitas One', cursive;
+  color: #dc143c;
+}
+
+.cover h1:hover {
+  transform: scale(1.2);
+}
+
+.cover p {
+  font-size: 1.2em;
+}
+</style>
