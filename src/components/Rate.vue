33a6614eb48833e47b27bea583899ca0ea21fce2
root <root@DESKTOP-4I5C2KQ.localdomain>
2021-01-11 22:21:22 -0500

    Crear componente imagen de calificación del comic con vuetify

---
+++ src/components/Rate.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-rating
+    v-model="rating"
+    background-color="purple lighten-3"
+    color="orange"
+    size="64"
+  ></v-rating>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rating: 2,
+  }),
+};
+</script>
